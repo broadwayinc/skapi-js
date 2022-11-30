decabf92d3ec7464c256f098fac128b84a21cab6
baksagimm <baksa@broadwayinc.com>
2022-12-01 03:17:43 +0900

    fixed webpack outputs

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
 const path = require('path');
-module.exports = {
+module.exports = [{
+    // cdn
     mode: 'production',
     target: 'web',
-    // target: 'node',
-    // externals: [nodeExternals()], // removes node_modules from your final bundle
     entry: './js/Api.js',
     output: {
-        // path: path.join(__dirname, 'bundle'),
         filename: 'skapi.js',
         libraryTarget: 'umd'
-        // libraryTarget: 'module'
     },
-    // experiments: {
-    //     outputModule: true
-    // },
-    // optimization: {
-    //     minimize: true
-    // },
     devtool: 'source-map'
-};
+}, {
+    // webpack
+    mode: 'production',
+    target: 'web',
+    entry: './js/Api.js',
+    output: {
+        filename: 'skapi.module.js',
+        libraryTarget: 'module'
+    },
+    experiments: {
+        outputModule: true
+    },
+    devtool: 'source-map'
+}];
