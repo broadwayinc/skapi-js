5f505c757cdcc79b515a6618e03dd09bd5078753
dev <dev@broadwayinc.com>
2022-11-30 18:39:21 +0000

    version update

--- deploy.py
+++ deploy.py
@@ -1,6 +1,18 @@
 import json
 import os
 
-with open('./package.json','r') as p:
+with open('./package.json', 'r') as p:
     package = json.loads(p.read())
-    os.system('npm publish')
+    ret = os.system('npm publish')
+    
+    if ret != 0:
+        print('==Failed to publish==')
+
+    else:
+        ret = os.system(
+            f'aws s3 sync ./dist s3://broadwayinc.dev/jslib/skapi/{package["version"]} --acl public-read')
+
+        if ret != 0:
+            print('==Failed to upload==')
+
+print('==END==')
