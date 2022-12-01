800cc58f031226a68d9abb0494d4674ed3177c62
dev <dev@broadwayinc.com>
2022-12-01 21:29:42 +0000

    deployment script

--- deploy.py
+++ deploy.py
@@ -1,24 +1,24 @@
 import json
 import os
 
+ret = os.system('git pull')
+    
+if ret != 0:
+    print('==Failed to pull==')
+
 with open('./package.json', 'r') as p:
     package = json.loads(p.read())
-    ret = os.system('git pull')
-        
-    if ret != 0:
-        print('==Failed to pull==')
+
+    ret = os.system('npm publish')
     
+    if ret != 0:
+        print('==Failed to publish==')
+
     else:
-        ret = os.system('npm publish')
-        
+        ret = os.system(
+            f'aws s3 sync ./dist s3://broadwayinc.dev/jslib/skapi/{package["version"]} --acl public-read')
+
         if ret != 0:
-            print('==Failed to publish==')
-
-        else:
-            ret = os.system(
-                f'aws s3 sync ./dist s3://broadwayinc.dev/jslib/skapi/{package["version"]} --acl public-read')
-
-            if ret != 0:
-                print('==Failed to upload==')
+            print('==Failed to upload==')
 
 print('==END==')
