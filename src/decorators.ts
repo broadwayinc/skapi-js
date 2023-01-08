9fedd1c366fc1ffb8736e21b7333a1f771d9b3b1
baksagimm <baksa@broadwayinc.com>
2023-01-09 08:51:01 +0900

    updates

--- src/decorators.ts
+++ src/decorators.ts
@@ -4,66 +4,65 @@ import { MD5 } from './utils';
 
 function formResponse() {
     // wraps methods that requires form handling
-    return function (target: object, propertyKey: string, descriptor: PropertyDescriptor) {
+    return function (target: object, propertyKey: string, descriptor: any) {
         const fn = descriptor.value;
 
         descriptor.value = function (...arg: any[]) {
             let form: Form | Record<string, any> = arg[0];
             let option: FormCallbacks = arg?.[1] || {};
+            let routeWithDataKey = true;
+            let formEl = null;
+            let actionDestination = '';
+
+            if (form instanceof SubmitEvent) {
+                form.preventDefault();
+
+                let currentUrl = window.location.href;
+                formEl = form.target as HTMLFormElement;
+                let href = new URL(formEl.action);
+                actionDestination = href.href;
+
+                if (!formEl.action || href.href === currentUrl) {
+                    routeWithDataKey = false;
+                }
+            }
 
             const handleResponse = (response: any) => {
                 if (option?.response) {
-                    if (typeof option.response === 'function') {
-                        // return response callback
-                        option.response(response);
-                        return response;
-                    } else {
+                    if (typeof option.response === 'function')
+                        return option.response(response);
+                    else
                         throw new SkapiError('Callback "response" should be type: function.', { code: 'INVALID_PARAMETER' });
+                }
+
+                if (formEl) {
+                    if (routeWithDataKey) {
+                        window.sessionStorage.setItem(`${this.service}:${MD5.hash(actionDestination)}`, JSON.stringify(response));
+                        window.location.href = actionDestination;
                     }
                 }
 
-                if (!(form instanceof HTMLFormElement)) {
-                    // return if is not form element
-                    return response;
-                }
-
-                // form element action
-                let currentUrl = window.location.href;
-
-                let href = new URL(form.action);
-                if (href.href !== currentUrl) {
-                    let response_key = MD5.hash(form.action);
-                    let timestamp = Date.now().toString();
-
-                    window.sessionStorage.setItem(response_key, JSON.stringify({ [timestamp]: response }));
-                    href.searchParams.set(response_key, timestamp);
-                    window.location.href = href.href;
-                }
-                else {
-                    return response;
-                }
+                // return if is not form element
+                return response;
             };
 
             let response: any;
             function handleError(err: any) {
                 let is_err = err instanceof Error ? err : new SkapiError(err);
                 if (option?.onerror) {
-                    if (typeof option.onerror === 'function') {
-                        // onerror callback
+                    if (typeof option.onerror === 'function')
                         return option.onerror(is_err);
-                    } else {
+                    else
                         throw new SkapiError('Callback "onerror" should be type: function.', { code: 'INVALID_PARAMETER' });
-                    }
                 }
 
-                return is_err;
+                throw is_err;
             }
 
             try {
                 // execute
                 response = fn.bind(this)(...arg);
             }
-
             catch (err) {
                 let is_err = handleError(err);
                 if (is_err instanceof Error) {
@@ -75,20 +74,15 @@ function formResponse() {
 
             if (response instanceof Promise) {
                 // handle promise
-                return new Promise((res, rej) => {
-                    response
-                        .then((r: any) => {
-                            res(handleResponse(r));
-                        })
-                        .catch((err: any) => {
-                            err = handleError(err);
-                            if (err instanceof Error) {
-                                rej(err);
-                            } else {
-                                res(err);
-                            }
-                        });
-                });
+                return (async () => {
+                    try {
+                        let resolved = await response;
+                        return handleResponse(resolved);
+                    }
+                    catch (err) {
+                        return handleError(err);
+                    }
+                })();
             }
 
             else {
