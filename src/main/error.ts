866378c3de2f33ee124b4539e6e45078bf18789f
baksagimm <baksa@broadwayinc.com>
2023-02-23 12:46:53 +0900

    updated error handler

--- src/main/error.ts
+++ src/main/error.ts
@@ -8,15 +8,21 @@ export default class SkapiError extends Error {
             cause?: Error;
         }) {
 
-        if (error instanceof Error) {
-            super(error.message || 'Something went wrong.');
-            this.cause = error;
-            this.name = error.name;
-            if (error.hasOwnProperty('code')) {
-                this.code = (error as any).code;
+        if (Array.isArray(error) && error.length <= 2) {
+            super(error[1] || 'Something went wrong.');
+            this.name = error[0] || "SKAPI";
+            this.code = error[0] || "ERROR";
+
+            if (options) {
+                if (options.code) {
+                    this.code = options.code;
+                }
+
+                if (options.cause) {
+                    this.cause = options.cause;
+                }
             }
         }
-
         else if (typeof error === 'string') {
             super(error || 'Something went wrong.');
             this.name = "SKAPI";
@@ -30,6 +36,14 @@ export default class SkapiError extends Error {
                 if (options.cause) {
                     this.cause = options.cause;
                 }
+            }
+        }
+        else if (error instanceof Error) {
+            super(error.message || 'Something went wrong.');
+            this.cause = error;
+            this.name = error.name;
+            if (error.hasOwnProperty('code')) {
+                this.code = (error as any).code;
             }
         }
     }
