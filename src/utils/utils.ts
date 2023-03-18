abf0aed5af155784f0e8c594099e1825069e42b0
baksagimm <baksa@broadwayinc.com>
2023-03-18 21:48:02 +0900

    added updateFiles

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -159,6 +159,18 @@ class MD5 {
     }
 }
 
+function generateRandom(length=6) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
 function extractFormMeta(form: Form<any>) {
     // creates meta object to post
 
@@ -317,5 +329,6 @@ function extractFormMeta(form: Form<any>) {
 
 export {
     extractFormMeta,
-    MD5
+    MD5,
+    generateRandom
 };
