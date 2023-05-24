fed60a4b2f76bb9cf6192f869d9b900ed7d44629
dev <dev@broadwayinc.com>
2023-05-24 06:26:07 +0000

    update

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -61,7 +61,7 @@ import {
 
 export default class Skapi {
     // current version
-    version = '0.1.57';
+    version = '0.1.58';
 
     // privates
     private __disabledAccount: string | null = null;
