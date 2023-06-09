108cfa296b2e46003f954ff0c5dfe4c43812b906
dev <dev@broadwayinc.com>
2023-06-09 09:40:34 +0000

    revert restrict admin private record post

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -61,7 +61,7 @@ import {
 
 export default class Skapi {
     // current version
-    version = '0.1.62';
+    version = '0.1.63';
 
     // privates
     private __disabledAccount: string | null = null;
