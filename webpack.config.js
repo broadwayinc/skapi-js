f6ce6123bafbb62e98e47a8003d3d09c688282c0
dev <dev@broadwayinc.com>
2023-08-09 15:14:57 +0000

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
 const path = require('path');
-module.exports = [{
-    // cdn
-    mode: 'production',
-    target: 'web',
-    entry: './js/Main.js',
-    output: {
-        filename: 'skapi.js',
-        libraryTarget: 'umd'
+module.exports = [
+    {
+        // cdn
+        mode: 'production',
+        target: 'web',
+        entry: './js/Main.js',
+        output: {
+            filename: 'skapi.js',
+            libraryTarget: 'umd'
+        },
+        devtool: 'source-map'
     },
-    devtool: 'source-map'
-},
-{
-    // webpack
-    mode: 'production',
-    target: 'web',
-    entry: './js/Main.js',
-    output: {
-        filename: 'skapi.module.js',
-        libraryTarget: 'module'
-    },
-    experiments: {
-        outputModule: true
-    },
-    devtool: 'source-map'
-}];
+    // {
+    //     // webpack
+    //     mode: 'production',
+    //     target: 'web',
+    //     entry: './js/Main.js',
+    //     output: {
+    //         filename: 'skapi.module.js',
+    //         libraryTarget: 'module'
+    //     },
+    //     experiments: {
+    //         outputModule: true
+    //     },
+    //     devtool: 'source-map'
+    // }
+];
