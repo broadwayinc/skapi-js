3189be9cdd4b84e436ebf8dbf461be0dde4d61e5
dev <dev@broadwayinc.com>
2023-09-17 14:07:29 +0000

    support for admin create

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -78,7 +78,7 @@ import {
 
 export default class Skapi {
     // current version
-    version = '1.0.0-alpha.27';
+    version = '1.0.0-alpha.28';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
