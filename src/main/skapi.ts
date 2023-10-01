7e843c2bb3be510bd211037644eb56dcd39848d7
dev <dev@broadwayinc.com>
2023-10-01 05:03:07 +0000

    fixed post record

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -79,7 +79,7 @@ import {
 
 export default class Skapi {
     // current version
-    version = '1.0.0-alpha.50';
+    version = '1.0.0-alpha.51';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
