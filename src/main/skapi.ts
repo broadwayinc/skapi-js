45af4b9c1413f0987c8863c4abef8705acead01c
dev <dev@broadwayinc.com>
2023-10-10 14:22:44 +0000

    fixed cdn endpoint

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -79,7 +79,7 @@ import {
 
 export default class Skapi {
     // current version
-    version = '1.0.0';
+    version = '1.0.1';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
