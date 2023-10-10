bf3549077b5dd1a6c2a33c3177eb9bd75b5b04eb
dev <dev@broadwayinc.com>
2023-10-10 14:35:27 +0000

    latest update

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -79,7 +79,7 @@ import {
 
 export default class Skapi {
     // current version
-    version = '1.0.2';
+    version = '1.0.3';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
