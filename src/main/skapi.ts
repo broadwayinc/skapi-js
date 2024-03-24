a8507fa3104f87fd0dd1665eb1841525e7dc31bb
dev <dev@broadwayinc.com>
2024-03-24 12:46:12 +0000

    fixed clientSecretRequest

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -94,7 +94,7 @@ import {
 } from '../utils/utils';
 export default class Skapi {
     // current version
-    version = '1.0.66';
+    version = '1.0.67';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
