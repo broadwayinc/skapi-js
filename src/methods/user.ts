6311729e5b52ae360b038d2610770468b14135b6
dev <dev@broadwayinc.com>
2024-06-12 22:49:40 +0000

    update

--- src/methods/user.ts
+++ src/methods/user.ts
@@ -890,7 +890,7 @@ export async function createAccount(
             Value: this.owner
         }),
         new CognitoUserAttribute({
-            Name: 'custom:signup',
+            Name: 'custom:signup_key',
             Value: signup_key
         })
     ];
