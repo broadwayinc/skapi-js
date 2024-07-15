8645271605be7f6530cfc49bc9f46dd50e9852d0
dev <dev@broadwayinc.com>
2024-07-15 03:19:21 +0000

    admin can upload private

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -95,7 +95,7 @@ import {
 } from '../utils/utils';
 export default class Skapi {
     // current version
-    version = '1.0.104';
+    version = '1.0.105';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
