d4a8ce0256e0fd2d5e5431068efa692339a55744
dev <dev@broadwayinc.com>
2024-08-07 03:12:03 +0000

    fixed form action

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -95,7 +95,7 @@ import {
 } from '../utils/utils';
 export default class Skapi {
     // current version
-    version = '1.0.116';
+    version = '1.0.117';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
