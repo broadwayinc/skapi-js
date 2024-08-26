ced8b472ef24ece1569a44ad4c2f8b70b546655e
dev <dev@broadwayinc.com>
2024-08-26 13:54:51 +0000

    clientSecretRequest now accpets form

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -95,7 +95,7 @@ import {
 } from '../utils/utils';
 export default class Skapi {
     // current version
-    version = '1.0.129';
+    version = '1.0.130';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
@@ -408,7 +408,8 @@ export default class Skapi {
     }): Promise<UserProfile> {
         return jwtLogin.bind(this)(params);
     }
-
+    
+    @formHandler()
     clientSecretRequest(params: {
         url: string;
         clientSecretName: string;
