4a1321f7425a411c607b7082b7d253009e22147c
dev <dev@broadwayinc.com>
2024-09-02 07:29:40 +0000

    endpoint fix

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -96,7 +96,7 @@ import {
 } from '../utils/utils';
 export default class Skapi {
     // current version
-    version = '1.0.141';
+    version = '1.0.142';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
