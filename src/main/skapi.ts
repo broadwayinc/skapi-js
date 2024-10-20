c7dafc6abf05fe824a0b8bde655c98ff26aa2fe1
dev <dev@broadwayinc.com>
2024-10-20 18:47:33 +0000

    removes cache promise on network error

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -96,7 +96,7 @@ import {
 } from '../utils/utils';
 export default class Skapi {
     // current version
-    private __version = '1.0.151';
+    private __version = '1.0.152';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
