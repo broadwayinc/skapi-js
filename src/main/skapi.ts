0573489c85c30ffc10347dce5a620752791fa8e1
gyupp <kkb75281@gmail.com>
2024-10-25 16:04:25 +0900

    change

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -105,7 +105,7 @@ import {
 } from '../methods/admin';
 export default class Skapi {
     // current version
-    private __version = '1.0.156';
+    private __version = '1.0.157';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
