20fbdcd4eb4731031a1885f72ec4443b446ffc97
dev <dev@broadwayinc.com>
2024-11-01 09:20:21 +0000

    falsey profile params defaults to empty string

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -106,7 +106,7 @@ import {
 } from '../methods/admin';
 export default class Skapi {
     // current version
-    private __version = '1.0.164';
+    private __version = '1.0.165';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
