689c617335694f8bcc8fd37f73e5425b6f9f2b47
dev <dev@broadwayinc.com>
2024-11-16 21:33:05 +0000

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = [
     {
         // cdn
@@ -9,7 +10,15 @@ module.exports = [
             filename: 'skapi.js',
             libraryTarget: 'umd'
         },
-        devtool: 'source-map'
+        devtool: 'source-map',
+        plugins: [
+            new BundleAnalyzerPlugin({
+                analyzerMode: 'static',
+                reportFilename: 'bundle-report.html',
+                openAnalyzer: false,
+                excludeAssets: [/node_modules/]
+              })
+        ],
     },
     {
         // webpack
@@ -36,6 +45,70 @@ module.exports = [
         experiments: {
             outputModule: true
         },
-        devtool: 'source-map'
+        devtool: 'source-map',
+        plugins: [
+            new BundleAnalyzerPlugin({
+                analyzerMode: 'static',
+                reportFilename: 'bundle-report-module.html',
+                openAnalyzer: false,
+                excludeAssets: [/node_modules/]
+              })
+        ],
     }
 ];
+
+// module.exports = 
+//     {
+//         // cdn
+//         mode: 'production',
+//         target: 'web',
+//         entry: './js/Main.js',
+//         output: {
+//             filename: 'skapi.js',
+//             libraryTarget: 'umd'
+//         },
+//         devtool: 'source-map',
+//         plugins: [
+//             new BundleAnalyzerPlugin({
+//                 analyzerMode: 'static',
+//                 reportFilename: 'bundle-report.html',
+//                 openAnalyzer: false,
+//                 excludeAssets: [/node_modules/]
+//               })
+//         ],
+//     }
+//     // {
+//     //     // webpack
+//     //     mode: 'production',
+//     //     target: 'web',
+//     //     // entry: './js/Main.js',
+//     //     entry: './src/Main.ts',
+//     //     output: {
+//     //         filename: 'skapi.module.js',
+//     //         libraryTarget: 'module'
+//     //     },
+//     //     module: {
+//     //         rules: [
+//     //             {
+//     //                 test: /\.tsx?$/,
+//     //                 use: 'ts-loader',
+//     //                 exclude: /node_modules/,
+//     //             },
+//     //         ],
+//     //     },
+//     //     resolve: {
+//     //         extensions: ['.tsx', '.ts', '.js'],
+//     //     },
+//     //     experiments: {
+//     //         outputModule: true
+//     //     },
+//     //     devtool: 'source-map',
+//     //     plugins: [
+//     //         new BundleAnalyzerPlugin({
+//     //             analyzerMode: 'static',
+//     //             reportFilename: 'bundle-report.html',
+//     //             openAnalyzer: false,
+//     //             excludeAssets: [/node_modules/]
+//     //           })
+//     //     ],
+//     // };
