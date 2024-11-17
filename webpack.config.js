275ca23f7929691e2963d2d1451308deaef6ed9f
dev <dev@broadwayinc.com>
2024-11-17 18:44:44 +0000

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,13 @@ const { BannerPlugin } = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 // Read the LICENSE file
 const license = fs.readFileSync(path.resolve(__dirname, 'LICENSE'), 'utf8').replace('[xxxx]', new Date().getFullYear());
+
 module.exports = [
     {
         // cdn
         mode: 'production',
         target: 'web',
-        entry: './js/Main.js',
+        entry: './src/Main.ts',
         output: {
             filename: 'skapi.js',
             libraryTarget: 'umd'
@@ -34,16 +35,14 @@ ${license}
         ],
     },
 
-
     {
         // webpack
         mode: 'production',
         target: 'web',
         entry: './src/Main.ts',
         output: {
-            filename: 'skapi.esm.js',
-            libraryTarget: 'module',
-            path: path.resolve(__dirname, 'dist')
+            filename: 'skapi.mjs',
+            libraryTarget: 'module'
         },
         module: {
             rules: [
@@ -86,9 +85,8 @@ ${license}
         target: 'node',
         entry: './src/Main.ts',
         output: {
-            filename: 'skapi.cjs.js',
-            libraryTarget: 'commonjs2',
-            path: path.resolve(__dirname, 'dist')
+            filename: 'skapi.cjs',
+            libraryTarget: 'commonjs2'
         },
         module: {
             rules: [
