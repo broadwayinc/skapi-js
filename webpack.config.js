208b8c5d7ab7183be0dc010a1cd48a72b7d65f05
dev <dev@broadwayinc.com>
2024-11-17 16:23:57 +0000

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
+const fs = require('fs');
+const { BannerPlugin } = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// Read the LICENSE file
+const license = fs.readFileSync(path.resolve(__dirname, 'LICENSE'), 'utf8').replace('[xxxx]', new Date().getFullYear());
 module.exports = [
     {
         // cdn
@@ -17,18 +21,29 @@ module.exports = [
                 reportFilename: 'bundle-report.html',
                 openAnalyzer: false,
                 excludeAssets: [/node_modules/]
-              })
+              }),
+              new BannerPlugin({
+                banner: `
+/**
+ * @license
+${license}
+ */
+                `.trim(),
+                raw: true
+            })
         ],
     },
+
+
     {
         // webpack
         mode: 'production',
         target: 'web',
-        // entry: './js/Main.js',
         entry: './src/Main.ts',
         output: {
-            filename: 'skapi.module.js',
-            libraryTarget: 'module'
+            filename: 'skapi.esm.js',
+            libraryTarget: 'module',
+            path: path.resolve(__dirname, 'dist')
         },
         module: {
             rules: [
@@ -52,63 +67,58 @@ module.exports = [
                 reportFilename: 'bundle-report-module.html',
                 openAnalyzer: false,
                 excludeAssets: [/node_modules/]
-              })
+            }),
+            new BannerPlugin({
+                banner: `
+/**
+ * @license
+${license}
+ */
+                `.trim(),
+                raw: true
+            })
+        ],
+    },
+
+    {
+        // CommonJS build
+        mode: 'production',
+        target: 'node',
+        entry: './src/Main.ts',
+        output: {
+            filename: 'skapi.cjs.js',
+            libraryTarget: 'commonjs2',
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        devtool: 'source-map',
+        plugins: [
+            new BundleAnalyzerPlugin({
+                analyzerMode: 'static',
+                reportFilename: 'bundle-report-commonjs.html',
+                openAnalyzer: false,
+                excludeAssets: [/node_modules/]
+            }),
+            new BannerPlugin({
+                banner: `
+/**
+ * @license
+${license}
+ */
+                `.trim(),
+                raw: true
+            })
         ],
     }
 ];
-
-// module.exports = 
-//     {
-//         // cdn
-//         mode: 'production',
-//         target: 'web',
-//         entry: './js/Main.js',
-//         output: {
-//             filename: 'skapi.js',
-//             libraryTarget: 'umd'
-//         },
-//         devtool: 'source-map',
-//         plugins: [
-//             new BundleAnalyzerPlugin({
-//                 analyzerMode: 'static',
-//                 reportFilename: 'bundle-report.html',
-//                 openAnalyzer: false,
-//                 excludeAssets: [/node_modules/]
-//               })
-//         ],
-//     }
-//     // {
-//     //     // webpack
-//     //     mode: 'production',
-//     //     target: 'web',
-//     //     // entry: './js/Main.js',
-//     //     entry: './src/Main.ts',
-//     //     output: {
-//     //         filename: 'skapi.module.js',
-//     //         libraryTarget: 'module'
-//     //     },
-//     //     module: {
-//     //         rules: [
-//     //             {
-//     //                 test: /\.tsx?$/,
-//     //                 use: 'ts-loader',
-//     //                 exclude: /node_modules/,
-//     //             },
-//     //         ],
-//     //     },
-//     //     resolve: {
-//     //         extensions: ['.tsx', '.ts', '.js'],
-//     //     },
-//     //     experiments: {
-//     //         outputModule: true
-//     //     },
-//     //     devtool: 'source-map',
-//     //     plugins: [
-//     //         new BundleAnalyzerPlugin({
-//     //             analyzerMode: 'static',
-//     //             reportFilename: 'bundle-report.html',
-//     //             openAnalyzer: false,
-//     //             excludeAssets: [/node_modules/]
-//     //           })
-//     //     ],
-//     // };
