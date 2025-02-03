016187f41a1076a853a94fc6d1d0064a52c03136
dev <dev@broadwayinc.com>
2025-02-03 05:23:42 +0000

    update

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -125,7 +125,7 @@ import{
 } from '../methods/vivian';
 export default class Skapi {
     // current version
-    private __version = '1.0.202';
+    private __version = '1.0.203';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
