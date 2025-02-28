c318f9be427e17aa12ced31531fab78df02fabc0
dev <dev@broadwayinc.com>
2025-02-28 12:26:01 +0000

    bump version to 1.0.222 in package.json

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -131,7 +131,7 @@ import {
 } from '../methods/vivian';
 export default class Skapi {
     // current version
-    private __version = '1.0.221';
+    private __version = '1.0.222';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
