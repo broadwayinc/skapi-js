ccaefdf2a4607b6de06ffdf8559b194594055a20
dev <dev@broadwayinc.com>
2025-04-21 07:06:57 +0000

    chore: bump version to 1.0.230 in Skapi class

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -132,7 +132,7 @@ import {
 } from '../methods/vivian';
 export default class Skapi {
     // current version
-    private __version = '1.0.229';
+    private __version = '1.0.230';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
