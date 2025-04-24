c39502d65fd50ee7b61ae759f9d531cb4cf82548
dev <dev@broadwayinc.com>
2025-04-24 09:28:41 +0000

    chore: bump version to 1.0.235 in package.json and update internal version reference in Skapi class

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -133,7 +133,7 @@ import {
 } from '../methods/vivian';
 export default class Skapi {
     // current version
-    private __version = '1.0.234';
+    private __version = '1.0.235';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
