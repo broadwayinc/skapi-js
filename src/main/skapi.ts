62689ff27295bc520721004b6a1bbc82bd736a0e
dev <dev@broadwayinc.com>
2025-05-01 10:01:49 +0000

    fix: update version to 1.0.241-beta.0 in package.json and skapi.ts; refactor connection state tracking in realtime.ts

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -133,7 +133,7 @@ import {
 } from '../methods/vivian';
 export default class Skapi {
     // current version
-    private __version = '1.0.240';
+    private __version = '1.0.241';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
