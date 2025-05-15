32b4fca3d42b1e309cdd756d814c81b63083e822
dev <dev@broadwayinc.com>
2025-05-15 08:17:03 +0000

    fix: update version to 1.0.244 in package.json and skapi.ts

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -133,7 +133,7 @@ import {
 } from '../methods/vivian';
 export default class Skapi {
     // current version
-    private __version = '1.0.243';
+    private __version = '1.0.244';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
