9645a7ad362dd8b3cc13090464a94464f1248aea
dev <dev@broadwayinc.com>
2025-06-22 08:21:22 +0000

    chore: update version to 1.0.247 in package.json and skapi.ts

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -135,7 +135,7 @@ import {
 } from '../methods/vivian';
 export default class Skapi {
     // current version
-    private __version = '1.0.244';
+    private __version = '1.0.247';
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
