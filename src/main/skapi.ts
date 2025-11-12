5b591e49d8af65e66ef6514c84461142e09da41a
dev <dev@broadwayinc.com>
2025-11-12 09:28:32 +0000

    fix: update version to 1.1.7-beta.0 in package.json and skapi.ts; update queuecumber dependency

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -136,7 +136,7 @@ import {
 
 export default class Skapi {
     // current version
-    private __version = "1.1.6";
+    private __version = "1.1.7-beta.0";
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
