7773ba42c405a475042d840d2fc6c329e5c29a6c
dev <dev@broadwayinc.com>
2025-11-12 07:54:20 +0000

    fix: update version to 1.1.6 in package.json and skapi.ts

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -135,7 +135,7 @@ import {
 
 export default class Skapi {
     // current version
-    private __version = "1.1.6-beta.6";
+    private __version = "1.1.6";
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
