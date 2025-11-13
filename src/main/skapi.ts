d4efca72a63db77f401ef4d89ebf0df7af6d0c63
dev <dev@broadwayinc.com>
2025-11-13 04:35:33 +0000

    fix: update version to 1.1.8 in README.md and skapi.ts; document terminatePendingRequests feature

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -136,7 +136,7 @@ import {
 
 export default class Skapi {
     // current version
-    private __version = "1.1.7";
+    private __version = "1.1.8";
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
