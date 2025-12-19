ea64b25c69ace5c4c08b4f5c000e9f81b5ea02e3
dev <dev@broadwayinc.com>
2025-12-19 10:07:13 +0000

    fix: update version to 1.2.6 in package.json and skapi.ts; add optional chaining in dopamine function

--- src/main/skapi.ts
+++ src/main/skapi.ts
@@ -150,7 +150,7 @@ type Options = {
 
 export default class Skapi {
     // current version
-    private __version = "1.2.5";
+    private __version = "1.2.6";
     service: string;
     owner: string;
     session: Record<string, any> | null = null;
--- src/methods/vivian.ts
+++ src/methods/vivian.ts
@@ -33,9 +33,11 @@ export async function dopamine(params){
     
     let response = await request.bind(this)('dopamine', params, {auth: true});
     
-    let message = response.previous_message.message
-    let name = response.previous_message.name
-    alert(`${name} said ${message}`)
+    let message = response?.previous_message?.message;
+    let name = response?.previous_message?.name;
+
+    if(message && name)
+        alert(`${name} said ${message}`)
 
     window.location.href = response.video
 }
